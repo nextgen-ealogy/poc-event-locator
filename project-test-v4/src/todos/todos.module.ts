6b020cba5909530e5df1e318587fb3b7e250f1bf
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-06-09 00:19:17 +0200

    Create todos.module.ts

---
+++ project-test-v4/src/todos/todos.module.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { TodosController } from './todos.controller';
+import { TodosService } from './todos.service';
+
+@Module({
+  controllers: [TodosController],
+  providers: [TodosService]
+})
+export class TodosModule {}
