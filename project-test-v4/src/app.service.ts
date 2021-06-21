3a77936a34d256c87871a53783591bedccf39045
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-06-21 09:15:29 +0200

    Delete app.service.ts

--- project-test-v4/src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Hello World!  try : http://localhost:3000/todos/';
-  }
-}
