29c4010726ac7170ce2f5bcf14a7d45fec9d608d
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-06-21 09:15:48 +0200

    Delete template.controller.ts

--- project-test-v4/src/template/template.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('template')
-export class TemplateController {
-  @Get()
-  findAll(): string {
-    return 'This action returns all cats';
-  }
-}
