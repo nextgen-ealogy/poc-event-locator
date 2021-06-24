eb1adf98fda1d98f6b7f94e7c500974dbc59e650
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2021-06-24 21:00:43 +0200

    fix: solve query

--- express dummy-v3/SearchEngine.js
+++ express dummy-v3/SearchEngine.js
@@ -1,64 +1,39 @@
-const { Client } = require('@elastic/elasticsearch');
-const client = new Client({ node: 'http://localhost:9200' });
+const { Client } = require("@elastic/elasticsearch");
+const client = new Client({ node: "http://localhost:9200" });
 
-const search = async (x1,y1,x2,y2) => {
+const search = async (x1, y1, x2, y2) => {
   const hits = [];
 
   // only string values are searchable
   const searchResult = await client
     .search({
-      index: 'events',
+      index: "events",
       body: {
-          "query": {
-            "geo_bounding_box": {
-              "location": {
-                "top_left": {
-                  "lat": x1,
-                  "lon":y1
-                },
-                "bottom_right": {
-                  "lat": x2,
-                  "lon": y2
-                }
-              }
-            }
-          }
+        query: {
+          geo_bounding_box: {
+            location: {
+              top_left: {
+                lat: parseFloat(x1),
+                lon: parseFloat(y1),
+              },
+              bottom_right: {
+                lat: parseFloat(x2),
+                lon: parseFloat(y2),
+              },
+            },
+          },
         },
       },
-    )
+    })
+    .catch((e) => console.log("err", e));
+  console.log(JSON.stringify(searchResult));
 
-    // .search({
-    //   index: 'events',
-    //   body: {
-    //          "pin": {
-    //           "location": {
-    //               "lat": x1,
-    //               "lon":y1
-    //           }
-    //         }
-    //     },
-    //   },
-    // )
-
-
-    // .search({
-    //   index: 'events',
-    //   body: {
-    //     "text": "Geo-point as an object",
-    //           "location": {
-    //               "lat": x1,
-    //               "lon":y1
-    //           }
-    //     },
-    //   },
-    // )
-    .catch((e) => console.log('err', e));
   if (
-    search &&
-    search.body &&
-    search.body.hits &&
-    search.body.hits.hits &&
-    search.body.hits.hits.length > 0
+    searchResult &&
+    searchResult.body &&
+    searchResult.body.hits &&
+    searchResult.body.hits.hits &&
+    searchResult.body.hits.hits.length > 0
   ) {
     hits.push(...searchResult.body.hits.hits);
   }
@@ -70,5 +45,5 @@ const search = async (x1,y1,x2,y2) => {
 };
 
 module.exports = {
-  search
+  search,
 };
