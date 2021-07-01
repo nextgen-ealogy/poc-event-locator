e0b23da963ffc6ad7e96a4dbc7951b9d58e03e75
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-07-01 19:43:58 +0200

    Create App.js

---
+++ react-leaflet-API/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Component } from 'react';
+import "./App.css";
+import Maps from "./component/Map";
+// import "../src/data/data.json"
+
+// console.log des coordonnées API
+/////////////////////////////////////////////////
+// paris :
+// fetch('http://localhost:3001/search?x1=49.28&y1=1.07&x2=47.78&y2=3.57')
+
+// lille :
+fetch('http://localhost:3001/search?x1=50.77&y1=2.89&x2=50.428&y2=3.27')
+
+.then((response) => {
+  return response.json()
+})
+.then((result) => {
+  console.log('coordonnée API :')
+  console.log(result)
+})
+
+class App extends Component {
+  // Méthode Post :
+  state = {
+    post: {}
+  }
+
+    render() {
+     //Affichage de la page : 
+    return (
+      <div className="App">
+      <h1>Map leaflet</h1>
+        <Maps />
+      </div>
+    );
+  }
+  }
+
+  export default App;
