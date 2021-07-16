c40f7a3dd1f65b6b70693c4954b6aa29dac67386
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-07-16 20:17:20 +0200

    Update Map.js

--- my-app/src/component/Map.js
+++ my-app/src/component/Map.js
@@ -1,3 +1,4 @@
+import { Bounds, LatLngBounds, latLngBounds, Map } from "leaflet";
 import React, { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
@@ -16,7 +17,25 @@ const Maps = () => {
   useEffect(() => {
     // TODO insérer les coordonnées à partir du state bounds
     console.log("bound", bounds)
-    fetch("http://localhost:3001/search?x1=50.77&y1=2.89&x2=50.428&y2=3.27")
+    console.log("bounds southWest", bounds._southWest)
+    console.log("bounds northEast", bounds._northEast)
+    // console.log("northEast Lat", bounds._northEast.lat , "northEast Lng", bounds._northEast.lng)
+    // console.log("southWest lat", bounds._southWest.lat, "southWest lng", bounds._southWest.lng )
+
+    // lille :
+    // fetch("http://localhost:3001/search?x1=50.77&y1=2.89&x2=50.428&y2=3.27")
+
+            // paris :
+    // fetch('http://localhost:3001/search?x1=49.28&y1=1.07&x2=47.78&y2=3.57')
+
+    
+      // fetch("http://localhost:3001/search?x1=49+bound._northEast.lat&y1=2.8+bound._northEast.lng&x2=48+bound._southWest.lat&y2=2+bound._southEast.lng")
+      // fetch("http://localhost:3001/search?x1=bound._northEast.lat&y1=bound._northEast.lng&x2=bound._southWest.lat&y2=bound._southEast.lng")
+
+      // le problème est que x1 et y1 doivent être = top_left mais le bounds._southEast ne peut pas être utilisé.
+
+    fetch("http://localhost:3001/search?x1=bounds._northEast.lat&y1=bounds._northEast.lng&x2=bounds._southWest.lat&y2=bounds._southWest.lng")
+
       .then((response) => {
         return response.json();
       })
@@ -32,8 +51,10 @@ const Maps = () => {
   const positions = post.hits.map((hit) => {
     return [hit._source.location.lat, hit._source.location.lon, hit._source.legend];
   });
-
   return (
+    <div className="Map">
+    {/* latitude: {bounds._southWest} */}
+    {console.log(bounds)}
     <MapContainer
       whenCreated={(map) => {
         setMap(map)
@@ -57,6 +78,7 @@ const Maps = () => {
         );
       })}
     </MapContainer>
+    </div>
   );
 };
 
