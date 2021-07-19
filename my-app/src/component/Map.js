71d0c4ca66f0e3f0bdaa9e1c1dabfb03f328133f
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-07-19 18:04:02 +0200

    Update Map.js

--- my-app/src/component/Map.js
+++ my-app/src/component/Map.js
@@ -15,43 +15,24 @@ const Maps = () => {
     }
   }, [map])
   useEffect(() => {
-    // TODO insérer les coordonnées à partir du state bounds
-    console.log("bound", bounds)
-    console.log("bounds southWest", bounds._southWest)
-    console.log("bounds northEast", bounds._northEast)
-    console.log("bounds zoom", bounds.zoom)
-    console.log("bounds northWest", bounds._northWest, "bounds southEast", bounds._southEast)
 
-    // console.log("northEast Lat", bounds._northEast.lat , "northEast Lng", bounds._northEast.lng)
-    // console.log("southWest lat", bounds._southWest.lat, "southWest lng", bounds._southWest.lng )
+    if(bounds._northEast && bounds._southWest){
+      const northWestX = bounds._northEast.lat;
+      const northWestY = bounds._southWest.lng;
 
-    // lille :
-    // fetch("http://localhost:3001/search?x1=50.77&y1=2.89&x2=50.428&y2=3.27")
+      const southEastX =  bounds._southWest.lat
+      const southEastY =  bounds._northEast.lng
 
-            // paris :
-    // fetch('http://localhost:3001/search?x1=49.28&y1=1.07&x2=47.78&y2=3.57')
+      fetch("http://localhost:3001/search?x1="+northWestX+"&y1="+northWestY+"&x2="+southEastX+"&y2="+southEastY)
+        .then((response) => {
+          return response.json();
+        })
+        .then((result) => {
+          setPost(result)
+        });
+    }
+ 
 
-    
-      // fetch("http://localhost:3001/search?x1=49+bound._northEast.lat&y1=2.8+bound._northEast.lng&x2=48+bound._southWest.lat&y2=2+bound._southEast.lng")
-      // fetch("http://localhost:3001/search?x1=bound._northEast.lat&y1=bound._northEast.lng&x2=bound._southWest.lat&y2=bound._southEast.lng")
-
-      // le problème est que x1 et y1 doivent être = top_left mais le bounds._northWest ne peut pas être utilisé.
-
-    // fetch("http://localhost:3001/search?x1=bounds._northEast.lat&y1=bounds._northEast.lng&x2=bounds._southWest.lat&y2=bounds._southWest.lng")
-    // fetch("http://localhost:3001/search?x1=bounds._northWest.lat&y1=bounds._northWest.lng&x2=bounds._southEast.lat&y2=bounds._southEast.lng")
-    // fetch("http://localhost:3001/search?x1="+bounds._northEast.lat+"&y1="+bounds._northEast.lng+"&x2="+bounds._southWest.lat+"&y2="+bounds._southWest.lng+"")
-    // fetch("http://localhost:3001/search?x1="+bounds._northWest.lat+"&y1="+bounds._northWest.lng+"&x2="+bounds._southEast.lat+"&y2="+bounds._southEast.lng+"")
-    // fetch("http://localhost:3001/search?x1="+bounds._northEast.lat+"&y1="+bounds._southWest.lng+"&x2="+bounds._southWest.lat+"&y2="+bounds._northEast.lng+"")
-    
-    fetch("http://localhost:3001/search?x1="+bounds._northEast.lat+"&y1="+bounds._northEast.lng+"&x2="+bounds._southWest.lat+"&y2="+bounds._southWest.lng+"")
-    fetch("http://localhost:3001/search?x1="+bounds._northEast.lat+"&y1="+bounds._southWest.lng+"&y2="+bounds._northEast.lng+"&x2="+bounds._southWest.lat+"")
-
-      .then((response) => {
-        return response.json();
-      })
-      .then((result) => {
-        setPost(result)
-      });
   }, [bounds]);
 
   if (!post.hits) {
