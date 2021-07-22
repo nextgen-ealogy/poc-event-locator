98f28a199fc8316c2b15215ffdb38a813a08697c
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-07-22 12:26:56 +0200

    Update server.js

--- express dummy-v3/server.js
+++ express dummy-v3/server.js
@@ -20,9 +20,34 @@ const client = new Client({
 });
 
 // Paris
+// const template = `{
+//   "events": [
+//     {{#repeat 5}}
+//     {
+//       "id": {{@index}},
+//       "timestampStart": "{{date '1900' '1950' 'DD/MM/YYYY'}}",
+//       "timestampEnd": "{{date '1951' '2022' 'DD/MM/YYYY'}}",
+//       "legend": "{{lorem}}",
+//       "dob": "{{date '1900' '2000' 'YYYY'}}",
+//       "address": "{{int 1 100}} {{street}}",
+//       "city": "{{city}}",
+//       "zipCode": "{{zipcode}}",
+//       "optedin": {{boolean}},
+//       "country": "{{country }}",
+//       "image": "img{{@index}}.png",
+//       "location": {
+//           "lat": {{float 48.5 49.26 '0.00'}},
+//           "lon": {{float 3.2 1.6 '0.00'}}
+//       }
+//     }
+//     {{/repeat}}
+//   ]
+// }`;
+
+// Lille
 const template = `{
   "events": [
-    {{#repeat 5}}
+    {{#repeat 2}}
     {
       "id": {{@index}},
       "timestampStart": "{{date '1900' '1950' 'DD/MM/YYYY'}}",
@@ -36,8 +61,8 @@ const template = `{
       "country": "{{country }}",
       "image": "img{{@index}}.png",
       "location": {
-          "lat": {{float 48.5 49.26 '0.00'}},
-          "lon": {{float 3.2 1.6 '0.00'}}
+        "lat": {{float 50.42 50.77 '0.00'}},
+        "lon": {{float 2.89 3.27 '0.00'}}
       }
     }
     {{/repeat}}
