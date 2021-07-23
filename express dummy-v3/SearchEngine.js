c537152857ca95f5c8a2afda176d79efaf27e8e9
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-07-23 12:07:55 +0200

    Update SearchEngine.js

--- express dummy-v3/SearchEngine.js
+++ express dummy-v3/SearchEngine.js
@@ -1,11 +1,12 @@
 const { Client } = require("@elastic/elasticsearch");
 const client = new Client({ node: "http://localhost:9200" });
 
-const search = async (x1, y1, x2, y2, d1, d2) => {
+const search = async (x1, y1, x2, y2, startDate, endDate) => {
   const hits = [];
+  
 
   // only string values are searchable
-  const searchResult = await client
+  // const searchResult = await client
     // .search({
     //   index: 'point',
     //   body: {
@@ -28,48 +29,95 @@ const search = async (x1, y1, x2, y2, d1, d2) => {
     // )
 
 // query geo_bounding_box & query
-    .search({
-      index: 'events',
-      body: {
-        "query": {
-          "bool": {
-            "must": {
-              "match_all": {}
+    // .search({
+    //   index: 'events',
+    //   body: {
+    //     "query": {
+    //       "bool": {
+    //         "must": {
+    //           "match_all": {}
+    //         },
+    //         "filter": {
+    //           "geo_bounding_box": {
+    //             "location": {
+    //               "top_left": {
+    //                 "lat": x1,
+    //                 "lon": y1
+    //               },
+    //               "bottom_right": {
+    //                 "lat": x2,
+    //                 "lon": y2
+    //               }
+    //             }
+    //           }
+    //         }
+    //       }
+    //     },
+    //     "query": {
+    //       "bool": {
+    //         "must": {
+    //           "match_all": {}
+    //         },
+    //         "filter": {
+    //           "range": {
+    //             "timestamp": {
+    //               "gte": d1,
+    //               "lte": d2
+    //             }
+    //           }
+    //         }
+    //       }
+    //     }
+    //     },
+    //   },
+    // )
+
+    const query = {
+      bool: {
+        filter: [
+          {
+            geo_bounding_box: {
+              location: {
+                top_left: {
+                  lat: parseFloat(x1),
+                  lon: parseFloat(y1),
+                },
+                bottom_right: {
+                  lat: parseFloat(x2),
+                  lon: parseFloat(y2),
+                },
+              },
             },
-            "filter": {
-              "geo_bounding_box": {
-                "location": {
-                  "top_left": {
-                    "lat": x1,
-                    "lon": y1
-                  },
-                  "bottom_right": {
-                    "lat": x2,
-                    "lon": y2
-                  }
-                }
-              }
-            }
           }
-        },
+        ]
+      }
+    }
+
+    if(startDate && endDate ){
+
+      query.bool.filter.push({
         "query": {
           "bool": {
-            "must": {
-              "match_all": {}
-            },
             "filter": {
               "range": {
                 "timestamp": {
-                  "gte": d1,
-                  "lte": d2
+                  "gte": startDate,
+                  "lte": endDate
                 }
               }
             }
           }
         }
+      })
+    }
+    const searchResult = await client
+      .search({
+        index: "events",
+        body: {
+          size : 100,
+          query: query
         },
-      },
-    )
+      })
 
 // query geo_bounding_box & query range timestamp
     // .search({
