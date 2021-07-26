eb44a5965d6a466e2a8d450363a0743c405b3f42
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-07-26 20:04:10 +0200

    Update SearchEngine.js

--- express dummy-v3/SearchEngine.js
+++ express dummy-v3/SearchEngine.js
@@ -1,7 +1,7 @@
 const { Client } = require("@elastic/elasticsearch");
 const client = new Client({ node: "http://localhost:9200" });
 
-const search = async (x1, y1, x2, y2, startDate, endDate) => {
+const search = async (x1, y1, x2, y2, timestampStart, timestampEnd) => {
   const hits = [];
   
 
@@ -93,17 +93,29 @@ const search = async (x1, y1, x2, y2, startDate, endDate) => {
       }
     }
 
-    if(startDate && endDate ){
+    // if(timestampStart && timestampEnd ){
+
+    //   query.bool.filter.push({
+    //     "range": {
+    //             "timestamp": {
+    //               "gte": Date.parse(timestampStart),
+    //               "lte": Date.parse(timestampEnd)
+    //             }
+    //           }
+    //   })
+    // }
+
+    // if(timestampStart && timestampEnd ){
 
       query.bool.filter.push({
         "range": {
                 "timestamp": {
-                  "gte": startDate,
-                  "lte": endDate
+                  "gte": Date.parse(timestampStart),
+                  "lte": Date.parse(timestampEnd),
                 }
               }
       })
-    }
+    // }
 
     const searchResult = await client
       .search({
