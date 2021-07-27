765f7f7419a6b73db2d2cec0d70d3f9888f897b4
MaximeB <77612947+Guliandre@users.noreply.github.com>
2021-07-27 12:22:00 +0200

    Update server.js

--- express dummy-v3/server.js
+++ express dummy-v3/server.js
@@ -25,8 +25,7 @@ const template = `{
     {{#repeat 10}}
     {
       "id": {{@index}},
-      "timestampStart": "{{date 1900 1950 'yyyy/MM/dd'}}",
-      "timestampEnd": "{{date 1951 2022 'yyyy/MM/dd'}}",
+      "timestamp": "{{date 1900 1950 'YYYY-MM-dd'}}",
       "legend": "{{lorem}}",
       "dob": "{{date '1900' '2000' 'YYYY'}}",
       "address": "{{int 1 100}} {{street}}",
@@ -50,8 +49,7 @@ const template = `{
 //     {{#repeat 10}}
 //     {
 //       "id": {{@index}},
-//       "timestampStart": "{{date '1900' '1950' 'DD/MM/YYYY'}}",
-//       "timestampEnd": "{{date '1951' '2022' 'DD/MM/YYYY'}}",
+//       "timestamp": "{{date 1900 1950 'YYYY-MM-dd'}}",
 //       "legend": "{{lorem}}",
 //       "dob": "{{date '1900' '2000' 'YYYY'}}",
 //       "address": "{{int 1 100}} {{street}}",
@@ -63,55 +61,6 @@ const template = `{
 //       "location": {
 //         "lat": {{float 50.42 50.77 '0.00'}},
 //         "lon": {{float 2.89 3.27 '0.00'}}
-//       }
-//     }
-//     {{/repeat}}
-//   ]
-// }`;
-
-// const template = `{
-//   "events": [
-//     {{#repeat 2}}
-//     {
-//       "id": {{@index}},
-//       "legend": "{{lorem}}",
-//       "dob": "{{date '1900' '2000' 'YYYY'}}",
-//       "address": "{{int 1 100}} {{street}}",
-//       "city": "{{city}}",
-//       "zipCode": "{{zipcode}}",
-//       "optedin": {{boolean}},
-//       "startDate": "{{date '1900' '1950' 'DD/MM/YYYY'}}",
-//       "endDate": "{{date '1951' '2022' 'DD/MM/YYYY'}}",
-//       "country": "{{country }}",
-//       "image": "img{{@index}}.png",
-//       "location": {
-//         "lat": {{float 48.5 49.26 '0.00'}},
-//         "lon": {{float 3.2 1.6 '0.00'}}
-//       }
-//     }
-//     {{/repeat}}
-//   ]
-// }`;
-
-// lille
-// const template = `{
-//   "events": [
-//     {{#repeat 2}}
-//     {
-//       "id": {{@index}},
-//       "startDate": "{{date '1900' '1950' 'DD/MM/YYYY'}}",
-//       "endDate": "{{date '1951' '2022' 'DD/MM/YYYY'}}",
-//       "legend": "{{lorem}}",
-//       "dob": "{{date '1900' '2000' 'YYYY'}}",
-//       "address": "{{int 1 100}} {{street}}",
-//       "city": "{{city}}",
-//       "zipCode": "{{zipcode}}",
-//       "optedin": {{boolean}},
-//       "country": "{{country }}",
-//       "image": "img{{@index}}.png",
-//       "location": {
-//           "lat": {{float 50.42 50.77 '0.00'}},
-//           "lon": {{float 2.89 3.27 '0.00'}}
 //       }
 //     }
 //     {{/repeat}}
@@ -136,34 +85,5 @@ app.get("/search", async (req, res) => {
   const data = await search(req.query.x1, req.query.y1, req.query.x2, req.query.y2, req.query.timestampStart, req.query.timestampEnd);
   res.json(data);
 });
-
-// // voir toutes la liste de toutes les template :
-// app.get('/template',
-//     (req, res) => {
-//         console.log("All template /")
-//         mysqlUtilities.getTemplate((result,error) => {
-
-//         if (!error){
-//             res.send(result)
-//             }else{
-//             res.status(500).send(error)
-//         }
-//     })
-// })
-
-//     //Récupère toutes les association template :
-//     getTemplate(callback){
-//       //On définis la connexion
-//       var connection = mysql.createConnection(config)
-//       //On lance la connexion
-//       connection.connect()
-//       //On envoi la query
-//       connection.query('SELECT * from template', (error, results) => {
-//           callback(results,error)
-//           })
-//       //On ferme la connexion
-//       connection.end()
-//       }
-
 app.listen(3001, () => console.log("server running at 3001"));
 
